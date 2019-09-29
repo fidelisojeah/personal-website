df7e9591b9004348c566e0732c0424dd5fdc9ee0
Fidelis Ojeah <fidelis_oj@yahoo.co.uk>
2019-09-29 09:16:56 +0100

    switching back to npm clean up modify postbuild

--- postbuild.js
+++ postbuild.js
@@ -2,17 +2,6 @@ const fs = require('fs');
 
 const paths = require('./paths');
 
-// fs.copySync(paths.appPublic, paths.appBuild, {
-//     dereference: true,
-//     filter: file => file !== paths.appHtml,
-// });
-
-// fs.copyFileSync(paths.appPublic, paths.appBuild, {
-//     dereference: true,
-//     filter: file => file !== paths.appHtml
-// })
-
 fs.readdirSync(paths.appPublic)
-    .filter(file => `${paths.appPublic}/${file}` !== paths.appHtml)
-    // .forEach((file) => console.log(file, 'FILE'))
-    .forEach((file) => fs.copyFileSync(`${paths.appPublic}/${file}`, `${paths.appBuild}/${file}`))
+    .filter((file) => `${paths.appPublic}/${file}` !== paths.appHtml)
+    .forEach((file) => fs.copyFileSync(`${paths.appPublic}/${file}`, `${paths.appBuild}/${file}`));
